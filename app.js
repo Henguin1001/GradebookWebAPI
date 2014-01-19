c719efdd47c0e76aa274e60e9d118b30a0c18f2d
Henry Troutman <henguin1001@gmail.com>
2014-01-18 23:16:25 -0800

    Retrying

--- app.js
+++ app.js
@@ -5,13 +5,14 @@
 
 var express = require('express');
 var routes = require('./routes');
+var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@
  * GET home page.
  */
 
-exports.main = function(req, res){
-	
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
 };
