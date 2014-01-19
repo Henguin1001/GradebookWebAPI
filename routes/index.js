e04a02f90e945633c41ab92da8d568b8e679d8d0
Henry Troutman <henguin1001@gmail.com>
2014-01-18 23:52:53 -0800

    Actually Working

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
-
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+var Gradebook = require('gradebook');
+exports.index = function(req, res) {
+	var gradebook = new Gradebook(req.query.username, req.query.password);
+	gradebook.startSession(function(err) {
+		if (!err) {
+			gradebook.getDefault(function(err, default_data) {
+				if (!err) {
+					gradebook.getGrades(function(err, grade_data) {
+						if (!err) {
+							gradebook.getAssignments(grade_data, function(err, grades_assignments_data) {
+								if (!err) {
+									res.end(JSON.stringify({
+										"default": default_data,
+										"courses": grades_assignments_data,
+									}));
+								} else res.end(JSON.stringify({
+									"default": default_data,
+									"courses": grade_data,
+									"error": "page not found"
+								}));
+							});
+						} else res.end(JSON.stringify({
+							"default": default_data,
+							"error": "page not found"
+						}));
+					});
+				} else res.end(JSON.stringify({
+					"error": "page not found"
+				}));
+			});
+		} else res.end(JSON.stringify({
+			"error": "username or password is incorrect"
+		}));
+	});
 };
